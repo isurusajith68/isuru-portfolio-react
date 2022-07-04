99c48cbe223e4a4b07d52ca979540a848b8fe74a
isuRuSajit <89340506+isuRuSajit@users.noreply.github.com>
2022-07-04 17:01:47 +0530

    fix button

--- src/components/HomeT.js
+++ src/components/HomeT.js
@@ -2,14 +2,14 @@ import "./HomeT.css";
 import Typewriter from "typewriter-effect";
 
 import React from "react";
-import img from "../assets/download.jpg";
+import download from "../assets/download.jpg";
 import { Link } from "react-router-dom";
 
 const HomeT = () => {
   return (
     <div className="homeT">
       <div className="mask">
-        <img className="download" src={img} alt="download" />
+        <img className="download" src={download} alt="download" />
       </div>
       <div className="content">
         <p className="tt">HI, I'M <span className="sp">Isuru Sajith</span></p>
@@ -23,8 +23,8 @@ const HomeT = () => {
           />
         </h1>
         <div>
-         <a href="Isuru Sajith-CV.pdf" download='Isuru CV.pdf'>
-          <button className="btn">Resume</button>
+          <a className="btn" href="Isuru Sajith-CV.pdf" download='Isuru CV.pdf'>
+          RESUME
          </a>
           <Link to="/Contact" className="btn-light">
             Contact
