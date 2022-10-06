38ac3dc2d9daf4e7c18d6f2cf09964c5ab445fc8
Isuru Sajith <isurusajith68@gmail.com>
2022-10-07 00:44:21 +0530

    test test

--- src/components/HomeT.js
+++ src/components/HomeT.js
@@ -12,21 +12,29 @@ const HomeT = () => {
         <img className="download" src={download} alt="download" />
       </div>
       <div className="content">
-        <p className="tt">HI, I'M <span className="sp">Isuru Sajith</span></p>
+        <p className="tt">
+          HI, I'M <span className="sp">Isuru Sajith</span>
+        </p>
         <h1>
           <Typewriter
             options={{
-              strings: ["Web Developer💻"],
+              // strings: ["Web Developer💻"],
               autoStart: true,
               loop: true,
             }}
           />
         </h1>
         <div>
-          <a className="btn" href="Isuru Sajith-CV.pdf" download='Isuru CV.pdf'>
-          RESUME
-         </a>
-          <Link to="form" className="btn-light" smooth={true} duration={500} style={{cursor:`pointer`}}>
+          <a className="btn" href="Isuru Sajith-CV.pdf" download="Isuru CV.pdf">
+            RESUME
+          </a>
+          <Link
+            to="form"
+            className="btn-light"
+            smooth={true}
+            duration={500}
+            style={{ cursor: `pointer` }}
+          >
             Contact
           </Link>
         </div>
