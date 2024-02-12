df7eb8d65ba54e8ca63ee8bc212e53f648548a6f
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-02-12 12:17:45 +0530

    add project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -6,58 +6,62 @@ import png from "../assets/codeVault.gif";
 import portfolio from "../assets/portfolio.gif";
 import coming from "../assets/87162442-bf3e8180-c2e7-11ea-9f2a-53a50306b7ce.gif";
 import authImage from "../assets/auths.gif";
-
+import chatapp from "../assets/chatapp.gif";
 
 export const projects = [
-    {
-        img: `url(${png})`,
-        name: "CodeVault (NextJS)",
-        gitUrl: `https://github.com/isurusajith68/CodeVault-NextJS`,
-        demo: `https://codevault68.netlify.app/`,
-    },
-    {
-        img: `url(${img})`,
-        name: "E Shop (React Native)",
-        gitUrl: `https://github.com/isurusajith68/Eshop_React_Native`,
-        demo: `https://github.com/isurusajith68/Eshop_React_Native`,
-    },
-    {
-        img: `url(${mern})`,
-        name: "Online Store (ReactJS Redux)",
-        gitUrl: `https://github.com/isurusajith68/ecommerce_site_redux`,
-        demo: `https://github.com/isurusajith68/ecommerce_site_redux`,
-    },
-    
+  {
+    img: `url(${png})`,
+    name: "CodeVault (NextJS)",
+    gitUrl: `https://github.com/isurusajith68/CodeVault-NextJS`,
+    demo: `https://codevault68.netlify.app/`,
+  },
+  {
+    img: `url(${chatapp})`,
+    name: "Chat App (ReactJS, NodeJS, Socket.io)",
+    gitUrl: `https://github.com/isurusajith68/chat-app-socket.io.git`,
+    demo: `https://chat-app-mi6i.onrender.com/`,
+  },
+  {
+    img: `url(${img})`,
+    name: "E Shop (React Native)",
+    gitUrl: `https://github.com/isurusajith68/Eshop_React_Native`,
+    demo: `https://github.com/isurusajith68/Eshop_React_Native`,
+  },
+  {
+    img: `url(${mern})`,
+    name: "Online Store (ReactJS Redux)",
+    gitUrl: `https://github.com/isurusajith68/ecommerce_site_redux`,
+    demo: `https://github.com/isurusajith68/ecommerce_site_redux`,
+  },
 
-
-    {
-        img: `url(${eco})`,
-        name: "Eco Mate Web App (MERN Stack)",
-        gitUrl: `https://github.com/isurusajith68/Ecomate_MERN`,
-        demo: `https://github.com/isurusajith68/Ecomate_MERN`,
-    },
-    {
-        img: `url(${authImage})`,
-        name: "User Authentication (Jwt ReactJs) ",
-        gitUrl: `https://github.com/isurusajith68/Login-OTP-Validation`,
-        demo: `https://github.com/isurusajith68/Login-OTP-Validation`,
-    },
-    {
-        img: `url(${and})`,
-        name: "Food Delivery App (Java)",
-        gitUrl: `https://github.com/isurusajith68`,
-        demo: `https://github.com/isurusajith68`,
-    },
-    {
-        img: `url(${portfolio})`,
-        name: "Portfolio (ReactJs)",
-        gitUrl: `https://github.com/isurusajith68/isuru-portfolio-react`,
-        demo: `https://isurusajith.netlify.app/`,
-    },
-    {
-        img: `url(${coming})`,
-        name: "coming soon",
-        gitUrl: `https://github.com/isurusajith68/`,
-        demo: `https://github.com/isurusajith68/`,
-    },
+  {
+    img: `url(${eco})`,
+    name: "Eco Mate Web App (MERN Stack)",
+    gitUrl: `https://github.com/isurusajith68/Ecomate_MERN`,
+    demo: `https://github.com/isurusajith68/Ecomate_MERN`,
+  },
+  {
+    img: `url(${authImage})`,
+    name: "User Authentication (Jwt ReactJs) ",
+    gitUrl: `https://github.com/isurusajith68/Login-OTP-Validation`,
+    demo: `https://github.com/isurusajith68/Login-OTP-Validation`,
+  },
+  {
+    img: `url(${and})`,
+    name: "Food Delivery App (Java)",
+    gitUrl: `https://github.com/isurusajith68`,
+    demo: `https://github.com/isurusajith68`,
+  },
+  {
+    img: `url(${portfolio})`,
+    name: "Portfolio (ReactJs)",
+    gitUrl: `https://github.com/isurusajith68/isuru-portfolio-react`,
+    demo: `https://isurusajith.netlify.app/`,
+  },
+  {
+    img: `url(${coming})`,
+    name: "coming soon",
+    gitUrl: `https://github.com/isurusajith68/`,
+    demo: `https://github.com/isurusajith68/`,
+  },
 ];
