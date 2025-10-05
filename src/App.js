4f101c8c05e364438b0ceaeee822f5b0a5bbd354
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2025-10-05 19:41:28 +0530

    Add comments for Navbar and Home Section in App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ function App() {
   return (
     <div>
       <div>
+        {/* Navbar */}
         <Navbar />
+        {/* Home Section */}
         <HomeT />
         <ScrollBtn />
         <AboutMe />
